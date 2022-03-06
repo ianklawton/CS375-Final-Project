dcaf4649a91d33e9c9ebd65d7d49f962dcd9031a
Ian Lawton <ianklawton@gmail.com>
2022-03-05 21:19:04 -0500

    Ian - Finished Login

--- setUpDatabase.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE valleytxt;
-\c valleytxt;
-CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(20), password CHAR(60));
-CREATE TABLE stats (username TEXT, heath TEXT, attack TEXT, defense TEXT, speed TEXT, inventory TEXT);
