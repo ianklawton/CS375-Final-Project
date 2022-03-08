69c4831d3489dd8a954f8111e35517a56237396e
Masenkas <masenkasper@gmail.com>
2022-03-08 14:28:09 -0500

    Server update

---
+++ testData.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password) VALUES ('test1', 'password');
+INSERT INTO stats (username, health, attack, defense, speed, inventory) VALUES ('test1', 100, 0, 2, 10, '{"inventory": ""}');
